4208e432f13d64e70e6dfb0b8ba31decf296c660
박현도 <117895948+hyundo0328@users.noreply.github.com>
2023-01-12 16:07:25 +0900

    백준 1000번

--- BaekJoon/src/Baekjoon/BJ1000.java
+++ BaekJoon/src/Baekjoon/BJ1000.java
@@ -1,10 +1,18 @@
 package Baekjoon;
 
+import java.util.Scanner;
+
 public class BJ1000 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		 
+		Scanner in = new Scanner(System.in);
+		int A = in.nextInt();
+		int B = in.nextInt();
+		
+		System.out.println(A+B);
+ 
+		in.close();
 	}
 
 }
