e731acd59963c1514ffca7510fd248973483d540
박현도 <117895948+hyundo0328@users.noreply.github.com>
2023-01-14 17:04:21 +0900

    23.01.14 (1)

---
+++ BaekJoon/src/Baekjoon/BJ2753.java
@@ -0,0 +1,18 @@
+package Baekjoon;
+
+import java.util.Scanner;
+
+public class BJ2753 {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+		int year = in.nextInt();
+		
+		if ((year%4==0 && year%100!=0)|| year%400==0) {
+			System.out.println("1");
+		} else {
+			System.out.println("0");
+		}
+	}
+}
