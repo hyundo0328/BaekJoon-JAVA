25c8dec5dd8c2d3b051a5ebed328e82e6ba4825f
박현도 <117895948+hyundo0328@users.noreply.github.com>
2023-01-16 20:51:10 +0900

    23.01.16 4-(2)

---
+++ BaekJoon/src/Baekjoon/BJ10818.java
@@ -0,0 +1,42 @@
+package Baekjoon;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BJ10818 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
+		
+		int num = Integer.parseInt(br.readLine());
+		int arr[] = new int[num];
+		int min=999999;
+		int max=-999999;
+		
+		st = new StringTokenizer(br.readLine());
+		for(int i=0;i<num;i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		for(int i=0;i<num;i++) {
+			if(min>arr[i]) {
+				min = arr[i];
+			}
+			if(max<arr[i]) {
+				max = arr[i];
+			}
+		}
+		bw.write(min+" "+max+"\n");
+		
+		br.close();
+		bw.flush();
+		bw.close();
+	}
+
+}
