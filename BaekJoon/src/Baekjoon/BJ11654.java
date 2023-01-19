df4b6828b7594e350edde65291bb5a50f0d682a5
박현도 <117895948+hyundo0328@users.noreply.github.com>
2023-01-20 01:15:34 +0900

    23.01.20 (1) char<->int buffer 안 되는 이유..?

---
+++ BaekJoon/src/Baekjoon/BJ11654.java
@@ -0,0 +1,17 @@
+package Baekjoon;
+
+import java.util.Scanner;
+
+public class BJ11654 {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+		char c = in.nextLine().charAt(0);
+		
+		System.out.println((int)c);
+		
+		in.close();
+	}
+
+}
