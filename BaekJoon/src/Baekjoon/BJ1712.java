394f9cec3be03e79e5cedaabc22e51d6614c63c1
박현도 <117895948+hyundo0328@users.noreply.github.com>
2023-01-24 16:42:59 +0900

    23.01.24 (1) 손익분기점

---
+++ BaekJoon/src/Baekjoon/BJ1712.java
@@ -0,0 +1,27 @@
+package Baekjoon;
+
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class BJ1712 {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		StringTokenizer st;
+		
+		String str = in.nextLine();
+		st = new StringTokenizer(str, " ");
+		int A = Integer.parseInt(st.nextToken());
+		int B = Integer.parseInt(st.nextToken());
+		int C = Integer.parseInt(st.nextToken());
+		
+		if(B>=C) {
+			System.out.println("-1");
+		} else {
+			System.out.println(A/(C-B)+1);
+		}
+		
+		in.close();
+	}
+
+}
