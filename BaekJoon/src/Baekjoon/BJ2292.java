e35d215390e8df903b2c7411aaf76ef514077a85
박현도 <117895948+hyundo0328@users.noreply.github.com>
2023-01-25 16:54:54 +0900

    23.01.25 (5)

--- BaekJoon/src/Baekjoon/BJ2292.java
+++ BaekJoon/src/Baekjoon/BJ2292.java
@@ -9,12 +9,12 @@ public class BJ2292 {
 		
 		int num = in.nextInt();
 		int count = 1;
-		int sum = 2;
+		int sum = 1;
 		
 		if(num==1) {
 			System.out.println(1);
 		} else {
-			while(sum<=num) {
+			while(sum<num) {
 				sum += 6*count;
 				count++;
 				//System.out.println("sum : "+sum+" count : "+count);
