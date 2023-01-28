00d85230afca9e0902cb2a68fbcddb3c53cd53a4
박현도 <117895948+hyundo0328@users.noreply.github.com>
2023-01-28 18:21:46 +0900

    23.01.28 (5)

---
+++ BaekJoon/src/Baekjoon/BJ4948.java
@@ -0,0 +1,42 @@
+package Baekjoon;
+
+import java.util.Scanner;
+
+public class BJ4948 {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+		while(true) {
+			int num = in.nextInt();
+			int count = 0;
+			
+			if(num == 0) {
+				break;
+			}
+			
+			for(int i=num+1;i<=2*num;i++) {
+				int flag = 0;
+				if(i==1) {
+					continue;
+				}
+				
+				for(int j=2;j<=Math.sqrt(i);j++) {
+					if(i%j==0) {
+						flag = 1;
+						break;
+					}
+				}
+				
+				if(flag == 0) {
+					// System.out.println("num : "+i);
+					count++;
+				}
+			}
+			System.out.println(count);
+		}
+		
+		in.close();
+	}
+
+}
