4771dc625961564ab92ea94468abccd0d7ecb4a4
박현도 <117895948+hyundo0328@users.noreply.github.com>
2023-01-28 17:52:11 +0900

    23.01.28 (3)

---
+++ BaekJoon/src/Baekjoon/BJ11653.java
@@ -0,0 +1,36 @@
+package Baekjoon;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class BJ11653 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int num = Integer.parseInt(br.readLine());
+		
+		while(num>1) {
+			int tmp = 2;
+			for(int i=2;i<=num;i++) {
+				if(num%i==0) {
+					tmp = i;
+					break;
+				}
+			}
+			
+			bw.write(tmp+"\n");
+			num /= tmp;
+			// bw.write("num : "+num+"\n\n");
+		}
+		
+		br.close();
+		bw.flush();
+		bw.close();
+	}
+
+}
