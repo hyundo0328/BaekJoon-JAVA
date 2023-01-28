408dce93bdac16db3e2132c6366577ce1997d989
박현도 <117895948+hyundo0328@users.noreply.github.com>
2023-01-29 01:42:00 +0900

    23.01.29 (5)

---
+++ BaekJoon/src/Baekjoon/BJ2587.java
@@ -0,0 +1,40 @@
+package Baekjoon;
+
+import java.util.Scanner;
+
+public class BJ2587 {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+		int arr[] = new int[5];
+		int average = 0;
+		
+		for(int i=0;i<5;i++) {
+			int num = in.nextInt();
+			arr[i] = num;
+			average += num;
+		}
+		
+		
+		// insertion sort
+		for(int i=1;i<5;i++) {
+			int tmp = arr[i];
+			int j;
+			
+			for(j=i;j>0;j--) {
+				if(arr[j-1]<tmp) {
+					break;
+				}
+				arr[j] = arr[j-1];
+			}
+			arr[j] = tmp;
+		}
+		
+		System.out.println(average/5);
+		System.out.println(arr[2]);
+		
+		in.close();
+	}
+
+}
