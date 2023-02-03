87511d255f297e446e88e1e2ee53f9c405230fb3
박현도 <117895948+hyundo0328@users.noreply.github.com>
2023-02-03 13:19:02 +0900

    23.02.03 (1)

---
+++ BaekJoon/src/Baekjoon/BJ10872.java
@@ -0,0 +1,25 @@
+package Baekjoon;
+
+import java.util.Scanner;
+
+public class BJ10872 {
+	
+	public static int factorial(int n) {
+		if(n == 0) {
+			return 1;
+		} else {
+			return n*factorial(n-1);
+		} 
+	}
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+		int num = in.nextInt();
+		
+		System.out.println(factorial(num));
+		
+		in.close();
+	}
+
+}
