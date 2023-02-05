f8c4cb15815e01b421faf9e1547ffab2a1664f1b
박현도 <117895948+hyundo0328@users.noreply.github.com>
2023-02-06 00:38:21 +0900

    23.02.06 (1) ArrayList 사용법

--- BaekJoon/src/Baekjoon/BJ2750.java
+++ BaekJoon/src/Baekjoon/BJ2750.java
@@ -5,6 +5,9 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 
 public class BJ2750 {
 
@@ -12,25 +15,38 @@ public class BJ2750 {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
-		int t = Integer.parseInt(br.readLine());
-		int arr[] = new int[t];
+//		int t = Integer.parseInt(br.readLine());
+//		int arr[] = new int[t];
+//		
+//		for(int i=0;i<t;i++) {
+//			int num = Integer.parseInt(br.readLine());
+//			arr[i] = num;
+//		}
+//		
+//		int i=0,j=0;
+//		for(i=1;i<t;i++) {
+//			int tmp = arr[i];
+//			for(j=i;j>0&&arr[j-1]>tmp;j--) {
+//				arr[j] = arr[j-1];
+//			}
+//			arr[j] = tmp;
+//		}
+//		
+//		for(i=0;i<t;i++) {
+//			bw.write(arr[i]+"\n");
+//		}
 		
-		for(int i=0;i<t;i++) {
-			int num = Integer.parseInt(br.readLine());
-			arr[i] = num;
+		ArrayList<Integer> arr = new ArrayList<>();
+		
+		int num = Integer.parseInt(br.readLine());
+		for(int i=0;i<num;i++) {
+			arr.add(Integer.parseInt(br.readLine()));
 		}
 		
-		int i=0,j=0;
-		for(i=1;i<t;i++) {
-			int tmp = arr[i];
-			for(j=i;j>0&&arr[j-1]>tmp;j--) {
-				arr[j] = arr[j-1];
-			}
-			arr[j] = tmp;
-		}
+		Collections.sort(arr);
 		
-		for(i=0;i<t;i++) {
-			bw.write(arr[i]+"\n");
+		for(int i=0;i<num;i++) {
+			bw.write(arr.get(i)+"\n");
 		}
 		
 		br.close();
