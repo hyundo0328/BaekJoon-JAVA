c39f49a638d215c8f244f4b262d5cf198e6c48c1
박현도 <117895948+hyundo0328@users.noreply.github.com>
2023-02-24 10:51:59 +0900

    23.02.24 (1)

---
+++ BaekJoon/src/Baekjoon/BJ11718.java
@@ -0,0 +1,28 @@
+package Baekjoon;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class BJ11718 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		while(true) {
+			String str = br.readLine();
+			if(str == null || str.isEmpty()) {
+				break;
+			}
+			bw.write(str+"\n");
+		}
+		
+		br.close();
+		bw.flush();
+		bw.close();
+	}
+
+}
