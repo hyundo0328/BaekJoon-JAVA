e6fef1a7621cc7228aba67c7aa59d0dad3bc2837
박현도 <117895948+hyundo0328@users.noreply.github.com>
2023-03-01 17:41:30 +0900

    23.03.01 (4)

---
+++ BaekJoon/src/Baekjoon/BJ24265.java
@@ -0,0 +1,29 @@
+package Baekjoon;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class BJ24265 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int num = Integer.parseInt(br.readLine());
+		long sum = 0;
+		
+		for(int i=1;i<=num-1;i++) {
+			sum += i;
+		}
+		bw.write(sum+"\n");
+		bw.write("2");
+		
+		br.close();
+		bw.flush();
+		bw.close();
+	}
+
+}
